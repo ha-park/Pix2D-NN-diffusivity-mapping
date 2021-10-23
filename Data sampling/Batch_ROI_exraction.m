7da7bf28cb545a1c59482708f931ed7eab17edd0
Ha H. Park <93060489+ha-park@users.noreply.github.com>
2021-10-23 16:49:21 -0700

    Add files via upload

---
+++ Data sampling/Batch_ROI_exraction.m
@@ -0,0 +1,23 @@
+DaxPath = uigetdir('G:\My Drive\Xu Lab\3. NN D map (10.1.2020 ~ )\Images','Select Dax batch folder to extract ROI');
+Daxfiles = dir(fullfile(DaxPath, '*.dax'));
+saveDir  = uigetdir('G:\My Drive\Xu Lab\3. NN D map (10.1.2020 ~ )\Data and results','Select path to save result');
+
+ROI_size = 7;
+frame_interval = 6000; 
+
+for i = 1:size(Daxfiles,1)
+   dax_name = [DaxPath '\' Daxfiles(i).name];
+   filehead = Daxfiles(i).name(1:end-4);
+   %mlist_name = [DaxPath '\' filehead '_list.bin'];
+   mlist_name = [DaxPath '\' filehead '_list-r.bin'];
+   
+   [roi_array, roi_xy, dim] = Extract_roi_avoid_vicinity_norm_insightbg_v3(dax_name,mlist_name,ROI_size,frame_interval);
+   
+   save ([saveDir '\ROI array_' filehead '_ROI size_' num2str(ROI_size) '.mat'],'roi_array','roi_xy','dim')
+
+end
+
+%copyfile([DaxPath '\simulation info.txt'],[saveDir '\simulation info.txt']);
+
+
+%clearvars -except DaxPath Daxfiles saveDir roi_array_v1 roi_xy_v1
---
+++ Data sampling/Extract_roi_avoid_vicinity_norm_insightbg_v3.m
@@ -0,0 +1,73 @@
+function [roi_array,roi_xy,dim] = Extract_roi_avoid_vicinity_norm_insightbg_v3(dax_name,mlist_name,roi_width,frame_interval)
+
+    %------------------------------------------------
+    % This function loads dax image and localization file to extract intensity
+    % into array
+    % This function is using ReadDax of matlab-storm published by Zhuanglab
+    % https://github.com/ZhuangLab/matlab-storm
+    %--------------------------------------------------------------------------
+    % Output: 
+    % roi_array: intenisty array - roi_width x roi_width x n array
+    % roi_xy: xy positions in pixel 
+    % dim: size of image in pixel
+    %--------------------------------------------------------------------------
+    % Input:
+    % dax_name: input dax image to read
+    % mlist_name: insight3 localization result file (.bin)
+    % roi_width: size of square-ROI to extract intensity
+    % frame_interval: size of frame step
+    %--------------------------------------------------------------------------
+    % Ha Park
+    % October 18, 2021
+    % Version 3.0
+    % Sampling background-subtracted and normalized ROI 
+    % save localization based on drift-corrected xy
+
+    %roi_half_width
+    rhw = (roi_width-1)/2;
+    
+    [movie, infoFile] = ReadDax(dax_name,'verbose',false,'startFrame',1,'endFrame',1);
+    r = readbinfileNXcYcZc(mlist_name);
+    dim = infoFile.frame_dimensions;
+    frame_length = double(r.TotalFrames);
+    
+    rx = ceil((r.x)-0.5); ry = ceil((r.y)-0.5); background = r.bg;    
+    indices = find(rx > rhw & rx <= dim(1) - rhw & ry <= dim(2) - rhw & ry > rhw);
+    roi_xy = [r.xc(indices) r.yc(indices)];
+    frame_list = r.frame(indices);
+
+    n_crop = size(indices,1);
+    temp_roi_array = zeros(roi_width,roi_width,n_crop);
+
+    for f = 1:frame_interval:frame_length
+        if f + frame_interval < frame_length
+            frame_f = f + frame_interval-1;
+        else
+            frame_f = frame_length;
+        end
+
+        movie = ReadDax(dax_name,'verbose',false,'startFrame',f,'endFrame',frame_f);
+        frame_ind = find(frame_list <= frame_f & frame_list >= f);
+        
+        for n = 1:size(frame_ind,1)
+            ind = indices(frame_ind(n));
+            x = rx(ind); y = ry(ind); fr = r.frame(ind);
+            same_frame_ind = find(r.frame == fr);
+            rx_window = rx(same_frame_ind); ry_window = ry(same_frame_ind);
+            if (abs(rx_window - x) < roi_width)'*(abs(ry_window - y) < roi_width) > 1
+                indices(frame_ind(n)) = 0;
+                continue
+            end
+            roi = single(movie(y-rhw:y+rhw,x-rhw:x+rhw,fr-f+1) - uint16(background(ind)));
+            roi = roi/sum(roi,'all');
+            temp_roi_array(:,:,frame_ind(n)) = roi;
+        end
+        disp(['Done extracting ' num2str(frame_f) ' frames'])
+    end
+    
+    ind = find(indices > 0);
+    roi_array = temp_roi_array(:,:,ind);
+    roi_xy = roi_xy(ind,:);
+    
+    disp(' ')
+    disp(['Extraction complete: Total ' num2str(size(ind,1)) ' ROI extracted'])
